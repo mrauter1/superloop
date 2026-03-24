46dd6ce004a9f9ec0e77453a28b7c179054f2690
Marcelo Rauter <marcelorauter@gmail.com>
2026-03-24 15:14:51 -0300

    Remove package shim and rely on root autoloop launcher

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 def test_package_imports():
     import autoloop.main
-    import autoloop.autoloop
     import autoloop.loop_control  # noqa: F401
 
-    assert autoloop.autoloop.main is autoloop.main.main
+    assert callable(autoloop.main.main)
